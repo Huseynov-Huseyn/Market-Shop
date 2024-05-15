3599b7e1d86f00cdb41aa983ebc96734fc5c857d
Hüseyn <huseynhuseyn343@gmail.com>
2024-05-15 15:27:02 +0400

    Project Created
    Created spring boot zip and import it in eclipse. Fixing pom.xml file and application.properties

---
+++ Codes/market-shop/src/main/java/az/developia/marketshop/MarketShopApplication.java
@@ -0,0 +1,15 @@
+package az.developia.marketshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketShopApplication.class, args);
+
+		System.out.println("Hello world!");
+	}
+
+}
---
+++ Codes/market-shop/src/test/java/az/developia/marketshop/MarketShopApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.marketshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
